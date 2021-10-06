1478f6f34c42079366ba3ac70be0dab151e4a94f
Валерий Кулаков <47772014+kulakovAngel@users.noreply.github.com>
2021-10-06 20:50:57 +0300

    Add deleting item from list feature (#5)
    * Add deleting item from list feature
    
    - Add `handleDeleteItem` method to the `App` component.
    - Transfer `handleDeleteItem` to the `List` component and then to
    `ListItem` as `onDelete` prop.
    - Create `ListItem` component and extends it from  `PureComponent` for
    rendering optimization.
    - Create the `button` DOM-element for deleting and add `onClick` event.
    
    * Fix render of `List` component.
    
     - Add conditional render of `<ul>` element.
    
    Co-authored-by: ValeryKulakov <kulakov.resurs@tut.by>

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,15 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { ListItem } from './ListItem';
 
 export class List extends Component {
     render() {
+        // if (!this.props.items.length) {
+        //     // return null;
+        //     return 'No items';
+        // }
         return (
-            <ul>
-                {
-                    this.props.items.map(item => (
-                        <li key={item.id}>{item.content}</li>
-                    ))
-                }
-            </ul>
+            !this.props.items.length ? (
+                <h2>No items</h2>
+            ) : (
+                <ul>
+                    {
+                        this.props.items.map(item => (
+                            <ListItem
+                                key={item.id}
+                                item={item}
+                                onDelete={this.props.handleDeleteItem}
+                            />
+                        ))
+                    }
+                </ul>
+            )
         );
     }
 };
